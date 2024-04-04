4ef9f30aafa9ca1e5e5559942fef15618d226761
sahitya <sahityaaryansingh@gmail.com>
2024-04-04 10:09:21 +0530

    modified the app.jks

--- chai-backend/src/app.js
+++ chai-backend/src/app.js
@@ -5,12 +5,16 @@ import express from 'express';
 
 const app = express();
 
+const port = process.env.PORT || 3020;
 
 
+// You can learn much about the docs of the "mongoose"
 
+
+// extra configuration settings for the "cors" middleware
 const corsConfig =  {
-    'origin': '*', //we can specify origin in the .env file as well
-    'credentials': true // by setting it true we can allow the client to icnlude the credentials (TLS client certificate),cookies, authentication headers) as headers in it's request 
+    'origin': '*' || process.env.CORS_ORIGIN, //we can specify origin in the .env file as well
+    'credentials': true // by setting it true we can allow the client to include the credentials (TLS client certificate),cookies, authentication headers) as headers in it's request 
 }
 
 // * by this we can configure our middlewares
@@ -23,7 +27,13 @@ app.use(express.json({limit: '16kb'}));
 
 
 // % 
-app.use(express.urlencoded({extended: true ,  limit: '16kb'}));
+app.use(express.urlencoded({extended: true ,  limit: '16kb'})); // extended : "true" will help the app to handle the nested objects as well.
 
 
-app.use(cookieParser());
+app.use(express.static("public")) // where we want to add some files or images
+
+
+app.use(cookieParser()); // this help me to access and modify the cookies of the user's browser
+
+
+export { app }
